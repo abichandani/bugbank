b9ab9976a9ad8b5976ea5b2ccbe52f9985644382
Hitesh Abichandani <offabichandani@gmail.com>
2021-01-09 03:17:41 +0530

    Add spring boot project

---
+++ src/main/java/com/bug/bank/BugbankApplication.java
@@ -0,0 +1,13 @@
+package com.bug.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugbankApplication.class, args);
+	}
+
+}
